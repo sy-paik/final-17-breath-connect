1f444aed9b434d565787000e2f6d92f07743ff20
sy-paik <sypaik.dev@gmail.com>
2024-04-03 16:48:34 +0900

    💄 design: 배경 색상과 로고 변경 * 불필요한 isLoading state 삭제 * 배경 색상 그라데이션 추가

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,71 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useRecoilValue } from 'recoil';
-import { isDarkModeState } from '../../atoms/StylesAtom';
-import SplashLogo from '../../assets/images/full-logo.svg';
-import SplashDark from '../../assets/images/login-logo-dark.svg';
-import SplashTitle from '../../assets/images/main-title.svg';
-import SplashTitleDark from '../../assets/images/main-title-dark.svg';
-import SnsLoginPage from '../LoginPage/SnsLoginPage/SnsLoginPage';
+import SvgSprite from 'assets/sprite/SvgSprite';
+import { useNavigate } from 'react-router-dom';
 import { fadeIn, zoomIn } from '../../styles/Animation';
+import { ROUTE } from 'constants/route';
 
 const SplashPage = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const isDarkMode = useRecoilValue(isDarkModeState);
-
+  const navigate = useNavigate();
   React.useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false);
+      navigate(ROUTE.LOGIN);
     }, 2500);
-
     return () => clearTimeout(timer);
-  }, []);
+  }, [navigate]);
 
   return (
-    <Container>
-      {isLoading ? (
-        <Main>
-          <Logo
-            src={isDarkMode ? SplashDark : SplashLogo}
-            alt='들숨날숨 로고'
-          />
-          <Title
-            src={isDarkMode ? SplashTitleDark : SplashTitle}
-            alt='들숨날숨'
-          />
-        </Main>
-      ) : (
-        <SnsLoginPage />
-      )}
-    </Container>
+    <SplashContainer>
+      <Main>
+        <SvgSprite
+          id='home-logo'
+          color='transparent'
+          width={188}
+          height={226}
+        />
+        <h2>
+          함께 달리는 즐거움을
+          <br />
+          나눌 수 있는 공간.
+        </h2>
+        <SvgSprite id='logo-text' color='white' width={120} />
+      </Main>
+    </SplashContainer>
   );
 };
 
 export default SplashPage;
 
-export const Container = styled.div``;
+export const SplashContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(#260a52, #6521d3);
+`;
+
 const Main = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 20px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`;
-
-const Logo = styled.img`
-  display: block;
-  width: 20rem;
-  height: 20rem;
-  margin-bottom: 3rem;
-  animation: ${fadeIn} 1s ease-in-out forwards;
-`;
-
-const Title = styled.img`
-  width: 13rem;
-  height: 4rem;
-  opacity: 0;
-  animation: ${fadeIn} 1s ease-in-out forwards,
-    ${zoomIn} 1s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.6s forwards;
+  h2 {
+    color: white;
+    font-size: 16px;
+    margin-bottom: -50px;
+    line-height: 24px;
+    text-align: center;
+    /* animation: ${fadeIn} 1s ease-in-out forwards; */
+    animation: ${fadeIn} 1s ease-in-out forwards,
+      ${zoomIn} 1s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.6s forwards;
+  }
 `;
