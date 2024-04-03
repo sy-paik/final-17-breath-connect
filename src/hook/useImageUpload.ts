856ebe2286deda7cef0d8bd4362131c6133aaab2
sy-paik <sypaik.dev@gmail.com>
2024-04-03 16:43:53 +0900

    💚 refactor: 이미지 업로드 쿼리 훅 적용 및 이미지 미리보기 변경 * 이미지 미리보기 FileReader에서 createObjectURL 로 변경

--- src/hook/useImageUpload.ts
+++ src/hook/useImageUpload.ts
@@ -1,24 +1,22 @@
-import { useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { postUploadProfile } from '../api/auth';
+import React from 'react';
 import imageCompression from 'browser-image-compression';
+import { usePostUploadProfile } from './api/auth/usePostUploadProfile';
 
-const useImageUpload = (prevImage) => {
-  const [previewImage, setPreviewImage] = useState(prevImage || '');
-  const [image, setImage] = useState(prevImage || '');
+const useImageUpload = (prevImage: string) => {
+  const [previewImage, setPreviewImage] = React.useState(prevImage || '');
+  const [image, setImage] = React.useState(prevImage || '');
 
-  const uploadImage = useMutation(['uploadImage'], postUploadProfile, {
-    onSuccess: (res) => {
-      console.log(res);
-      setImage(process.env.REACT_APP_API_URL + res.filename);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
+  const {
+    mutate: uploadProfile,
+    isSuccess,
+    data: res,
+  } = usePostUploadProfile();
 
-  const handleImage = async (e) => {
-    const file = e.target.files[0];
+  // handleImageChange 하면서 submit
+  const handleImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    console.log(e.target.files);
+
     const allowedExtensions = [
       'jpg',
       'gif',
@@ -28,39 +26,42 @@ const useImageUpload = (prevImage) => {
       'tif',
       'heic',
     ];
-    const fileExtension = file.name.split('.').pop().toLowerCase();
+    const fileExtension = file?.name.split('.').pop()?.toLowerCase();
 
-    if (allowedExtensions.includes(fileExtension)) {
-      try {
-        const options = {
-          maxSizeMB: 1,
-          maxWidthOrHeight: 800,
-          useWebWorker: true,
-        };
+    if (!fileExtension) {
+      console.error('파일 확장자를 추출할 수 없습니다.');
+      return;
+    }
 
-        const compressedImage = await imageCompression(file, options);
-        const compressedFile = new File(
-          [compressedImage],
-          compressedImage.name,
-          {
-            type: compressedImage.type,
-          }
-        );
-
-        const imgData = new FormData();
-        imgData.append('image', compressedFile);
-        await uploadImage.mutate(imgData);
-
-        const reader = new FileReader();
-        reader.readAsDataURL(compressedFile);
-        reader.onload = () => {
-          setPreviewImage(reader.result);
-        };
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
+    if (!allowedExtensions.includes(fileExtension)) {
       alert('유효하지 않은 파일 형식입니다.');
+      return;
+    }
+
+    try {
+      const options = {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 800,
+        useWebWorker: true,
+      };
+
+      if (file) {
+        // TODO) 이미지 리사이징에 시간이 소요되므로 변환과정 중임을 알리기 위한 Spinner 요소 추가
+        const compressedBlob: Blob = await imageCompression(file, options);
+        const compressedFile = new File([compressedBlob], file?.name, {
+          type: file.type,
+        });
+        uploadProfile(compressedFile);
+
+        // TODO) 이미지 업로드 성공 토스트 추가
+        isSuccess &&
+          setImage(process.env.REACT_APP_API_URL + res.data.filename);
+        const blobUrl = window.URL.createObjectURL(compressedFile);
+        console.log(blobUrl, 'blobUrl임!!!!');
+        setPreviewImage(blobUrl);
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
