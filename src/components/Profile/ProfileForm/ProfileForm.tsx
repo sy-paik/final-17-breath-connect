e5ddc7b99f33de5207876d61fadc2a837d581eb1
sy-paik <sypaik.dev@gmail.com>
2024-04-03 16:54:14 +0900

    💚 refactor: 이미지 업로드 훅과 회원가입 쿼리 훅 적용

--- src/components/Profile/ProfileForm/ProfileForm.tsx
+++ src/components/Profile/ProfileForm/ProfileForm.tsx
@@ -1,104 +1,82 @@
-// import React from 'react';
-// import { useLocation } from 'react-router-dom';
-// import Input from '../../common/Input/Input';
-// import Button from '../../common/Button/Button';
-// import ProfileImage from '../ProfileImage/ProfileImage';
-// import { useFieldController } from '../../../hook/useFieldController';
-// import { useForm } from 'react-hook-form';
-// import { PATTERN, MESSAGE } from '../../../constants/validation';
-// import useImageUpload from '../../../hook/useImageUpload';
+import React from 'react';
+import Input from '../../common/Input/Input';
+import Button from '../../common/Button/Button';
+import ProfileImage from '../ProfileImage/ProfileImage';
+import { LoginSignupRequest } from 'hook/api/auth/usePostLogin';
+import { usePostAccountnameValid } from 'hook/api/auth/usePostAccountnameValid';
+import { usePostSignup } from 'hook/api/auth/usePostSignup';
+import useImageUpload from 'hook/useImageUpload';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from 'constants/route';
 
-// const ProfileForm = ({ myInfo, accountname, isError, message, userEmail, userPassword, signup, edit }) => {
+const ProfileForm = ({ signupInfo }: { signupInfo: LoginSignupRequest }) => {
+  const { image, previewImage, handleImage } = useImageUpload('');
+  const [signupProfile, setSignupProfile] = React.useState({
+    username: '',
+    email: signupInfo.email,
+    password: signupInfo.password,
+    accountname: '',
+    intro: '',
+    image: image,
+  });
 
-//   const { control, getValues, handleSubmit, formState: { errors } } = useForm({
-//     mode:'onBlur',
-//     defaultValues: {
-//       username: myInfo?.user?.username || '',
-//       accountname: myInfo?.user?.accountname || '',
-//       intro: myInfo?.user?.intro || '',
-//     }
-//   })
+  const navigate = useNavigate();
 
-//   const prevImage = myInfo?.user?.image;
-//   const { image, previewImage, handleImage } = useImageUpload(prevImage ? prevImage : null);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSignupProfile({
+      ...signupProfile,
+      [name]: value,
+    });
+  };
 
-//   const usernameController = useFieldController('username', control, {
-//     required: MESSAGE.USERNAME.REQUIRED,
-//     pattern: {
-//       value: PATTERN.USERNAME,
-//       message: MESSAGE.USERNAME.PATTERN
-//     }
-//   });
+  const { mutate: accountnameValid } = usePostAccountnameValid();
+  const handleAccountnameValid = () => {
+    accountnameValid(signupProfile.accountname);
+  };
+  const { mutate: signup, isSuccess: signupSuccess } = usePostSignup();
+  const handleSignup = (e: React.FormEvent) => {
+    e.preventDefault();
+    signup(signupProfile);
+    signupSuccess && navigate(ROUTE.LOGIN);
+  };
 
-//   const accountnameController = useFieldController('accountname', control, {
-//     required: MESSAGE.ACCOUNTNAME.REQUIRED,
-//     pattern: {
-//       value: PATTERN.ACCOUNTNAME,
-//       message: MESSAGE.ACCOUNTNAME.PATTERN
-//     },
-//     onBlur: () => {
-//       validation();
-//     }
-//   });
+  return (
+    <form onSubmit={handleSignup}>
+      <ProfileImage previewImage={previewImage} handleImage={handleImage} />
+      <Input
+        label='사용자 이름'
+        id='username'
+        name='username'
+        value={signupProfile.username}
+        type='text'
+        placeHolder='2~10자 이내여야 합니다.'
+        onChange={handleChange}
+      />
+      <Input
+        label='계정 ID'
+        id='accountname'
+        name='accountname'
+        value={signupProfile.accountname}
+        type='text'
+        placeHolder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
+        onChange={handleChange}
+        onBlur={handleAccountnameValid}
+      />
+      <Input
+        label='소개'
+        id='intro'
+        name='intro'
+        value={signupProfile.intro}
+        type='text'
+        placeHolder='자신에 대해 소개해 주세요!'
+        onChange={handleChange}
+      />
+      <Button type='submit' size='lg'>
+        들숨날숨 시작하기
+      </Button>
+    </form>
+  );
+};
 
-//   const validation = () => {
-//     accountname(accountnameController.field.value);
-//   }
-
-//   const introController = useFieldController('intro', control, {});
-
-//   const location = useLocation();
-
-//    const onSubmit = async (data) => {
-//       const userData = {
-//         username: data.username,
-//         accountname: data.accountname,
-//         intro: data.intro || '',
-//         image: image || '',
-//       };
-//       if (location.pathname === '/signup/profile') {
-//         userData.email = userEmail;
-//         userData.password = userPassword;
-//        await signup(userData);
-//       } else {
-//         edit(userData);
-//       }
-//     }
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <ProfileImage
-//         previewImage={previewImage} handleImage={handleImage}
-//       />
-//         <Input
-//           label='사용자 이름'
-//           id='username'
-//           type='text'
-//           placeHolder='2~10자 이내여야 합니다.'
-//           isError={errors.username?.message}
-//           errorMsg={errors.username?.message}
-//           {...usernameController.field}
-//         />
-//         <Input
-//           label='계정 ID'
-//           id='accountname'
-//           type='text'
-//           placeHolder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
-//           isError={errors.accountname?.message || isError}
-//           errorMsg={errors.accountname?.message || isError && message}
-//           successMsg={!isError && message}
-//           {...accountnameController.field}
-//         />
-//           <Input
-//           label='소개'
-//           placeHolder='자신에 대해 소개해 주세요!'
-//           id='intro'
-//           type='text'
-//           {...introController.field}
-//           />
-//       <Button type='submit' size={'L'} text={'들숨날숨 시작하기'} isDisabled = {!getValues('username') || !getValues('accountname')} />
-//     </form>
-//   )
-// }
-
-// export default ProfileForm;
+export default ProfileForm;
