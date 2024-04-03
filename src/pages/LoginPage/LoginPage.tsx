d1c7e05f8483b6f5b852268193307de15dd875c1
sy-paik <sypaik.dev@gmail.com>
2024-04-03 16:50:39 +0900

    💚 refactor: 회원가입 이동 링크 추가

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// import { useRecoilValue } from 'recoil';
-// import { isDarkModeState } from '../../../atoms/StylesAtom';
-import LoginForm from "components/Login/LoginForm";
+import LoginForm from 'components/Login/LoginForm';
 import styled from 'styled-components';
-import SvgSprite from "assets/sprite/SvgSprite";
+import SvgSprite from 'assets/sprite/SvgSprite';
+import { Link } from 'react-router-dom';
+import { ROUTE } from 'constants/route';
 
 const LoginPage = () => {
   // const isDarkMode = useRecoilValue(isDarkModeState);
@@ -11,13 +11,17 @@ const LoginPage = () => {
     <LoginContainer>
       <LogoSection>
         <h1>
-          <SvgSprite id='logo-text' color="white" width={120} />
+          <SvgSprite id='logo-text' color='white' width={120} />
         </h1>
         <h2>함께 달리는 즐거움을 나눌 수 있는 공간</h2>
       </LogoSection>
       <LoginSection>
         <h3>이메일로 로그인</h3>
         <LoginForm />
+        <SignupSection>
+          <p>계정이 없으신가요?</p>
+          <SignupLink to={ROUTE.SIGNUP}>이메일로 회원가입</SignupLink>
+        </SignupSection>
       </LoginSection>
     </LoginContainer>
   );
@@ -25,10 +29,9 @@ const LoginPage = () => {
 
 export default LoginPage;
 
-
 const LogoSection = styled.section`
   width: 50%;
-  background-image: linear-gradient(#34116D, #6521D3);
+  background-image: linear-gradient(#260a52, #6521d3);
   position: relative;
   h1 {
     margin-top: 30%;
@@ -36,11 +39,15 @@ const LogoSection = styled.section`
   h2 {
     color: white;
   }
-  h1, h2 {
+  h1,
+  h2 {
     margin-left: 83px;
     font-size: 16px;
   }
-`
+  h3 {
+    font-size: ${({ theme }) => theme.fontSize['2xl']};
+  }
+`;
 
 const LoginContainer = styled.div`
   width: 100vw;
@@ -63,6 +70,22 @@ const LoginSection = styled.section`
   margin-top: 10%;
   gap: 10%;
   h3 {
-    font-size: large;
+    font-size: ${({ theme }) => theme.fontSize['2xl']};
   }
-`
+`;
+
+const SignupSection = styled.div`
+  text-align: center;
+  p {
+    font-size: ${({ theme }) => theme.fontSize.sm};
+    color: ${({ theme }) => theme.colors.textColor};
+    margin-bottom: 0.6rem;
+  }
+`;
+
+const SignupLink = styled(Link)`
+  font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.colors.textColor};
+  font-weight: 600;
+  text-decoration: underline;
+`;
